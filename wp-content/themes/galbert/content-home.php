b91e92ebe42fc619de58746b14b6c29bd08636d0
carlos (minimal) <padi_05@yahoo.com>
2017-09-19 22:12:20 -0400

    Home video title + player style fixes

--- wp-content/themes/galbert/content-home.php
+++ wp-content/themes/galbert/content-home.php
@@ -21,10 +21,11 @@
 	</div>
 
 	<section id="home-video" class="video-embed">
+		<h3 class="uppercase"><?php the_field('home_video_title'); ?></h3>
 		<div class="video-container"><?php the_field('home_video'); ?></div>
 		<svg class="play-icon svg-icon" viewBox="0 0 100 100" title="Play Video">
 			<circle class="circle" cx="50" cy="50" r="42"></circle>
-			<path class="triangle" d="M 30,20 L 70,50 L 30,80 Z"></path>
+			<path class="triangle" d="M 40,27 L 67,50 L 40,73 Z"></path>
 		</svg>
 	</section>
 	
